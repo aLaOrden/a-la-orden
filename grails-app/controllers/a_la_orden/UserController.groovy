3aa5311fdf16f10939e82f02105f5e9b69c412f5
gggonzalezg <gggonzalezg@unal.edu.co>
2016-04-01 16:06:57 -0500

    feat(User): búsqueda usuario con username(mejorado)

--- grails-app/controllers/a_la_orden/UserController.groovy
+++ grails-app/controllers/a_la_orden/UserController.groovy
@@ -11,7 +11,20 @@ class UserController extends RestfulController {
 
     def username () {
         try {
-            respond User.findByUsername(params.username as String)
+            def a = User.findByUsername(params.username as String)
+            def userInfo = [
+                    ["AVATAR: " , a.avatarLink],
+                    ["NOMBRE: " , a.firstName],
+                    ["APELLIDO: " , a.lastName],
+                    ["EMAIL: " , a.email],
+                    ["PHONE: " , a.phone],
+                    ["GENERO: " , a.gender],
+                    ["SCORES: " , a.scores],
+                    ["OFERTAS: " , a.offers],
+                    ["DEMANDAS: " , a.demands],
+                    ["FAVORITOS: " , a.favorites]
+            ]
+            respond userInfo
         }
         catch (Exception e) {
             response.setContentType("application/json")
@@ -19,3 +32,5 @@ class UserController extends RestfulController {
         }
     }
 }
+
+//[{"class":"a_la_orden.User","id":1,"admin":true,"avatarLink":null,"demands":[{"class":"a_la_orden.Demand","id":3}],"email":"maasencioh@gmail.com","favorites":[],"firstName":"Miguel","gender":"M","lastName":"Asencio","offers":[{"class":"a_la_orden.Offer","id":1}],"password":"12345","phone":null,"scores":[{"class":"a_la_orden.Score","id":1}],"username":"maasencioh"}]
