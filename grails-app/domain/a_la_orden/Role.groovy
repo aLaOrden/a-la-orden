616be9ba6d8a393cf3f89e3e1bb7032261f13636
erickvelasco11 <erickvelasco11@gmail.com>
2016-04-08 17:39:13 -0500

    Clases de dominio y controladores Role y UserRole

---
+++ grails-app/domain/a_la_orden/Role.groovy
@@ -0,0 +1,24 @@
+package a_la_orden
+
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
+@EqualsAndHashCode(includes='authority')
+@ToString(includes='authority', includeNames=true, includePackage=false)
+class Role implements Serializable {
+
+    String authority
+
+    Role(String authority) {
+        this()
+        this.authority = authority
+    }
+
+    static constraints = {
+        authority blank: false, unique: true
+    }
+
+    static mapping = {
+        cache true
+    }
+}
