1d2c8db3f6ab7ad74a6a7885601f36b6b3bf6781
maasencioh <maasencioh@gmail.com>
2016-05-09 13:42:29 -0500

    chore: Migra Service a Demand y Offer

--- grails-app/domain/a_la_orden/Tag.groovy
+++ grails-app/domain/a_la_orden/Tag.groovy
@@ -4,9 +4,14 @@ class Tag {
 
     String title
 
-    static belongsTo = Service
+    static hasMany = [
+            offers: Offer,
+            demands: Demand
+    ]
 
     static constraints = {
-        title nullable: false, size: 5..50
+        title   nullable: false, size: 5..50
+        offers  nullable: true
+        demands nullable: true
     }
 }
