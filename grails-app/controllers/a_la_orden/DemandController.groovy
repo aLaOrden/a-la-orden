b09456a8efeae996040742b952b56fa00c59f3da
erickvelasco11 <erickvelasco11@gmail.com>
2016-06-01 07:16:42 -0500

    Método title demanda y oferta arreglado

--- grails-app/controllers/a_la_orden/DemandController.groovy
+++ grails-app/controllers/a_la_orden/DemandController.groovy
@@ -59,14 +59,14 @@ class DemandController extends RestfulController {
                     ilike('title', '%' + params.title + '%')
                 }
             }
-            def allDemands = demands.collect {
+            def allDemands = demands.collectEntries() { Demand dmnd ->
                 [
-                        id         : demands.id[0],
-                        title      : demands.title[0],
-                        description: demands.description[0],
-                        deadline   : demands.deadline[0],
-                        state      : demands.state[0],
-                        solved     : demands.solved[0]
+                        id         : demands.id,
+                        title      : demands.title,
+                        description: demands.description,
+                        deadline   : demands.deadline,
+                        state      : demands.state,
+                        solved     : demands.solved
                 ]
             }
 
